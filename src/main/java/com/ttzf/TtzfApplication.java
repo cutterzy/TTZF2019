a8f5f9d94e6521cdc0c5cf08e8d6a21a27d2f8e4
cutterzy <cutterzy@qq.com>
2019-06-14 10:34:29 +0800

    test1

---
+++ src/main/java/com/ttzf/TtzfApplication.java
@@ -0,0 +1,13 @@
+package com.ttzf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtzfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtzfApplication.class, args);
+    }
+
+}
